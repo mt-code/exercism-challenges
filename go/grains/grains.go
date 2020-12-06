674cc5df6b8499aaee9b629b92fc0a9b54646e5f
mt-code <matthew.croston@btinternet.com>
2020-12-06 01:35:01 +0000

    Updated solution to use bitwise operators

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,26 +1,17 @@
 package grains
 
-import (
-	"errors"
-	"math"
-)
+import "errors"
 
 func Square(input int) (uint64, error) {
 	if input < 1 || input > 64 {
 		return 0, errors.New("the square number provided is not valid")
 	}
-	if input == 1 {
-		return 1, nil
-	}
 
-	return uint64(math.Pow(2, float64(input)-1)), nil
+	return uint64(1) << uint64(input-1), nil
 }
 
-func Total() (result uint64) {
-	for i := 1; i <= 64; i++ {
-		value, _ := Square(i)
-		result += value
-	}
-
-	return result
+func Total() uint64 {
+	// uint64(0) sets all the register bits to 0
+	// ^ = XOR operator which flips the bits to 1, which equals uint64 max value
+	return ^uint64(0)
 }
